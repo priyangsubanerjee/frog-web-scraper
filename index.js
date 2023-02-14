974757c512ab4a8bc170501e592224b08ded6513
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-14 20:10:54 +0530

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000 || process.env.PORT;
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cheerio = require("cheerio");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.get("/", async (req, res) => {
+  const axiosResponse = await axios.request({
+    method: "GET",
+    url: "https://www.coingecko.com/en/coins/froggies-token",
+  });
+  const $ = cheerio.load(axiosResponse.data);
+  const list = $(
+    "span[class='tw-text-gray-900 dark:tw-text-white tw-font-medium tw-mr-1']"
+  );
+
+  res.json({
+    circulatingSupply: parseInt(list[0].children[0].data.replace(/,/g, "")),
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
